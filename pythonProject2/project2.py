172f9c5b1b9f218858b5c2453330e314015adb33
Ali Rüveycan <113598241+aliruveycan@users.noreply.github.com>
2024-04-28 21:08:09 +0300

    Making QRCODE with Python
    Making QRCODE with Python

---
+++ pythonProject2/project2.py
@@ -0,0 +1,11 @@
+import qrcode
+import image
+
+qr = qrcode.QRCode(version=15,box_size=10,border=5)
+
+data = "Ali RUVEYCAN"
+
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image(fill="black",back_color = "white")
+img.save("qr.png")
